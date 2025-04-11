a4a3e140541ec849c23e9d8760bf3978f5acc193
BuildTools <florian.graziani@sfr.fr>
2025-04-11 14:23:14 +0200

    Changement du logo

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -34,7 +34,7 @@
 
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
-        <link rel="shortcut icon" href={{asset("logo.svg")}}>
+        <link rel="shortcut icon" href="{{asset("logo.svg")}}" type="image/x-icon">
 
         @routes
         @viteReactRefresh
